72d19bc150c9e85e04cb6db37e0fc093eaba09bf
danielnorato <danielsouza05@yahoo.com.br>
2021-01-28 17:09:45 -0300

    first js done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+var nome = "Daniel Souza";
+var idade = 33;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
+console.log(nome);
+console.log("Idade " + idade);
+console.log("Idade2 " + idade2);
+console.log(frase.replace("Japão", "Brasil"))
+console.log(frase.toUpperCase())
